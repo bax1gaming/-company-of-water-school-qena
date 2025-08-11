cde9673ed2c582a47c27f17d534f425c091acb88
bax1woa <asawalsae@gmail.com>
2025-08-11 14:31:09 +0300

    Fix corrupted TrainerDashboard.vue file

--- src/views/TrainerDashboard.vue
+++ src/views/TrainerDashboard.vue
@@ -1,1 +1,16 @@
-{"code":"rate-limited","message":"You have hit the rate limit. Please upgrade to keep chatting.","providerLimitHit":false,"isRetryable":true}
+<template>
+  <div class="trainer-dashboard">
+    <h1>Trainer Dashboard</h1>
+    <p>Dashboard content will be loaded here.</p>
+  </div>
+</template>
+
+<script setup>
+// Trainer dashboard logic will be implemented here
+</script>
+
+<style scoped>
+.trainer-dashboard {
+  padding: 20px;
+}
+</style>
